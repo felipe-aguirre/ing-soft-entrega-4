5f44a6435eeebc78a3d860e932e8677496b64831
Felipe Aguirre <felipe@pulsingstudio.com>
2021-07-31 23:25:27 -0400

    Version Quezada

--- api/ayudante/controllers/ayudante.js
+++ api/ayudante/controllers/ayudante.js
@@ -1,8 +1,67 @@
 'use strict';
-
+//http://ec2-184-72-113-92.compute-1.amazonaws.com:3000/assistant/manager-range?rut_manager={rut_manager}&range={range}")
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
+let reSingle = new RegExp('(=)(.*)');
+let reDoble = new RegExp('(=)(.*)&(.*)(=)(.*)');
+module.exports = {
+  async rut(ctx){
+    let entradaRut = ctx.req._parsedUrl.query.match(reSingle)[2]
+    console.log(entradaRut)
 
-module.exports = {};
+    let result = [{
+      'rut': '19746913', 
+      'nombre': 'Joaquin Caqueo',
+      'contacto': 'joaquin.caqueo@sansano.usm.cl',
+      'rango': 'B',
+      'rut_gestor': '20978123'
+    }]
+  return result
+  },
+
+  async range(ctx){
+    let entradaRange = ctx.req._parsedUrl.query.match(reSingle)[2]
+    console.log(entradaRange)
+
+    let result = [{
+      'rut': '19746913', 
+      'nombre': 'Joaquin Caqueo',
+      'contacto': 'joaquin.caqueo@sansano.usm.cl',
+      'rango': 'B',
+      'rut_gestor': '20978123'
+    }]
+  return result
+  },
+  async manager(ctx){
+    let entradaManager = ctx.req._parsedUrl.query.match(reSingle)[2]
+    console.log(entradaManager)
+
+    let result = [{
+    'rut': '19746913', 
+    'nombre': 'Joaquin Caqueo',
+    'contacto': 'joaquin.caqueo@sansano.usm.cl',
+    'rango': 'B',
+    'rut_gestor': '20978123'
+  }]
+  
+  return result
+  },
+  async managerRange(ctx){
+    let entradaManager = ctx.req._parsedUrl.query.match(reDoble)[2]
+    let entradaRange = ctx.req._parsedUrl.query.match(reDoble)[5]
+
+    console.log(entradaManager, entradaRange)
+
+    let result = [{
+      'rut': '19746913', 
+      'nombre': 'Joaquin Caqueo',
+      'contacto': 'joaquin.caqueo@sansano.usm.cl',
+      'rango': 'B',
+      'rut_gestor': '20978123'
+    }]
+  return result
+  }
+
+};
--- api/estudiante/controllers/estudiante.js
+++ api/estudiante/controllers/estudiante.js
@@ -1,8 +1,67 @@
 'use strict';
 
+// http://ec2-184-72-113-92.compute-1.amazonaws.com:3000/student/campus-career?campus={campus}&career={career}")
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
+let reSingle = new RegExp('(=)(.*)');
+let reDoble = new RegExp('(=)(.*)&(.*)(=)(.*)');
+module.exports = {
 
-module.exports = {};
+  async rut(ctx){
+    let entradaRut = ctx.req._parsedUrl.query.match(reSingle)[2]
+    console.log(entradaRut)
+
+    let result = {
+      "rut": "20208070",
+      "nombre": "Fernanda Avendano",
+      "contacto": "fernanda.avendano@sansano.usm",
+      "carrera": "73",
+      "campus": "CCC"
+  }
+  return result
+  },
+
+  async campus(ctx){
+    let entradaCampus = ctx.req._parsedUrl.query.match(reSingle)[2]
+    console.log(entradaCampus)
+
+    let result = {
+      "rut": "20208070",
+      "nombre": "Fernanda Avendano",
+      "contacto": "fernanda.avendano@sansano.usm",
+      "carrera": "73",
+      "campus": "CCC"
+  }
+  return result
+  },
+  async career(ctx){
+    let entradaCareer = ctx.req._parsedUrl.query.match(reSingle)[2]
+    console.log(entradaCareer)
+
+    let result = {
+      "rut": "20208070",
+      "nombre": "Fernanda Avendano",
+      "contacto": "fernanda.avendano@sansano.usm",
+      "carrera": "73",
+      "campus": "CCC"
+  }
+  return result
+  },
+  async campusCareer(ctx){
+    let entradaCampus = ctx.req._parsedUrl.query.match(reDoble)[2]
+    let entradaCareer = ctx.req._parsedUrl.query.match(reDoble)[5]
+
+    console.log(entradaCampus, entradaCareer)
+
+    let result = {
+      "rut": "20208070",
+      "nombre": "Fernanda Avendano",
+      "contacto": "fernanda.avendano@sansano.usm",
+      "carrera": "73",
+      "campus": "CCC"
+  }
+  return result
+  }
+};
